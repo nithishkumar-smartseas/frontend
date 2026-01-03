0272e730c6ca2c40a8f0b4f8e6217428408b26a3
nithishkumar-smartseas <nithishkumar@smartseas.ai>
2026-01-03 12:19:32 +0530

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.end('Hello from Frontend');
+  res.end('Hello from Frontend-01');
 }).listen(3000);
 
 console.log('Frontend running on port 3000');
