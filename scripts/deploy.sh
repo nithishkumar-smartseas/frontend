89197182d563471dce4c7e608b753a0a1fa741c8
hello-world <hello-world@gmail.com>
2026-01-03 21:21:52 +0530

    deployment files 2nd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,9 @@ echo "Logging in to ECR"
 aws ecr get-login-password --region us-east-1 \
 | docker login --username AWS --password-stdin 991940085316.dkr.ecr.us-east-1.amazonaws.com
 
-
+echo "Stopping old container (if exists)"
+docker stop frontend || true
+docker rm frontend || true
 
 echo "Pulling new image"
 docker pull 991940085316.dkr.ecr.us-east-1.amazonaws.com/frontend:latest
