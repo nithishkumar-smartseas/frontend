7c31d20dcd7491e21a422ddf35203d1ee9fd15bf
hello-world <hello-world@gmail.com>
2026-01-05 13:53:29 +0530

    new ports added

--- frontend.js
+++ frontend.js
@@ -4,4 +4,4 @@ http.createServer(function (req, res) {
   res.end('Hello from Frontend-01');
 }).listen(6000);
 
-console.log('Frontend running on port 3000');
+console.log('Frontend running on port 6000');
